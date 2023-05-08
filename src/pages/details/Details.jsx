58ad797f49a5f4e53ffff2f990be8b346ffebf9a
irwinekabakti <irwinekabakti@gmail.com>
2023-05-08 13:54:00 +0700

    addFeature:(details page)

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,16 +1,35 @@
-import { Fragment } from 'react'
-import './Details.scss'
-import Header from '../../components/header/Header'
-import Footer from '../../components/footer/Footer'
+import { Fragment } from "react";
+import { useParams } from "react-router";
+import "./Details.scss";
+import Header from "../../components/header/Header";
+import Footer from "../../components/footer/Footer";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideoSection from "./videoSection/VideoSection";
+import Similar from "./carousels/Similiar";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(
+    `/${mediaType}/${id}/credits`
+  );
+
   return (
     <Fragment>
       <Header />
-      <div>Details</div>
+      <div>
+        <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+        <Cast data={credits?.cast} loading={creditsLoading} />
+        <VideoSection data={data} loading={loading} />
+        <Similar mediaType={mediaType} id={id} />
+        <Recommendation mediaType={mediaType} id={id} />
+      </div>
       <Footer />
     </Fragment>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
